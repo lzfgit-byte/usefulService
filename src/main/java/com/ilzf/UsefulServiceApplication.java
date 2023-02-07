ff698a08142f4d0424567e828ed50c624b7401a3
lzfgit-byte <1807409742@qq.com>
2023-02-07 21:57:44 +0800

    init

--- src/main/java/com/ilzf/UsefulServiceApplication.java
+++ src/main/java/com/ilzf/UsefulServiceApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsefulServiceApplication {
 
 	public static void main(String[] args) {
+		long s = System.currentTimeMillis();
 		SpringApplication.run(UsefulServiceApplication.class, args);
+		long e = System.currentTimeMillis();
+		System.out.print("启动耗时:" + (e - s) / 1000 + "秒");
 	}
 
 }
