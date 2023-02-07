bea46dfa632ed15ee7642e674fb5a4223e48c9c0
lzfgit-byte <1807409742@qq.com>
2023-02-07 20:45:18 +0800

    init

---
+++ src/main/java/com/ilzf/UsefulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ilzf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsefulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsefulServiceApplication.class, args);
+	}
+
+}
