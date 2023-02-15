4e0c3f3788eba922f8b9ecbb7b7c4e1a59e9421c
lzfgit-byte <1807409742@qq.com>
2023-02-15 21:52:27 +0800

    feat:共享文件开发

--- src/main/java/com/ilzf/onlineShare/controller/OnlineShareController.java
+++ src/main/java/com/ilzf/onlineShare/controller/OnlineShareController.java
@@ -4,25 +4,31 @@ import cn.hutool.json.JSONObject;
 import com.ilzf.base.entity.ResultEntity;
 import com.ilzf.onlineShare.entity.OnlineShareEntity;
 import com.ilzf.utils.DataUtilILZF;
+import com.ilzf.utils.StringUtilIZLF;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/onlineShare")
 public class OnlineShareController {
 
-    @RequestMapping("/save")
-    public ResultEntity<?> saveData(@RequestBody String text, HttpServletRequest request) {
+    @PostMapping("/save")
+    public ResultEntity<?> saveData(@RequestBody Map<String, Object> params) {
+        String text = StringUtilIZLF.wrapperString(params.get("text"));
+        if (StringUtilIZLF.isBlankOrEmpty(text)) return ResultEntity.error();
         OnlineShareEntity entity = new OnlineShareEntity(text);
         boolean b = DataUtilILZF.saveData(entity);
         return b ? ResultEntity.success() : ResultEntity.error();
     }
+
     @RequestMapping("/loadData")
-    public ResultEntity<?> loadData(){
+    public ResultEntity<?> loadData() {
         JSONObject savedData = DataUtilILZF.getSavedData(OnlineShareEntity.class);
-        return savedData == null ? ResultEntity.success("",true) :ResultEntity.success((String) ((JSONObject)savedData.get("a")).get("text"),true);
+        return savedData == null ? ResultEntity.success("", true) : ResultEntity.success((String) ((JSONObject) savedData.get("a")).get("text"), true);
     }
 }
