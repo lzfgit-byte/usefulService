fdd926a17d8ee43b1790d755b244b8a75778ea83
lzfgit-byte <1807409742@qq.com>
2023-02-25 23:17:26 +0800

    feat:切换为slf4j

--- src/main/java/com/ilzf/utils/LogUtilILZF.java
+++ src/main/java/com/ilzf/utils/LogUtilILZF.java
@@ -1,26 +1,21 @@
 package com.ilzf.utils;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.io.FileUtil;
+import lombok.extern.slf4j.Slf4j;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-
+@Slf4j
 public class LogUtilILZF {
-    public static final String LOG_FILE = "log.txt";
-
     public static void log(Object o) {
-        String logPath = FileUtilILZF.getSaveDataPath() + "log.txt";
         String logStr = StringUtilIZLF.wrapperString(o);
         if (StringUtilIZLF.isBlankOrEmpty(logStr)) {
             log("记录空日志");
             return;
         }
-        FileUtil.appendString("[" + DateUtil.now() + "]  " + logStr + StringUtilIZLF.LINE_BREAK, logPath, StandardCharsets.UTF_8);
+        log.info(logStr);
     }
     public static void log(String ...logsStr){
         StringBuffer sb = new StringBuffer();
-        Arrays.stream(logsStr).forEach(log -> sb.append(log));
+        Arrays.stream(logsStr).forEach(sb::append);
         log(sb);
     }
 }
